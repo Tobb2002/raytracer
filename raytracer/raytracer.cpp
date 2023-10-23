4608ba91432d07899df2ba88ccdf4ee16fa4e287
Tobi <tobias.vonier@gmx.de>
2023-10-23 20:13:59 +0200

    added basic Vector class #10

--- raytracer/raytracer.cpp
+++ raytracer/raytracer.cpp
@@ -4,12 +4,17 @@
 
 #include <iostream>
 #include "./filehandler.h"
+#include "./vector.h"
 
 
 int main(void) {
   safe_picture("filename.ppm");
 
   init_picture();
+
+  Vector v = Vector(1,2,3);
+  
+  v.print();
 
   return 0;
 }
