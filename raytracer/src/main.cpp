6c17eaced606a0630f09e01969a80d281c361cc5
Tobi <tobias.vonier@gmx.de>
2024-02-01 16:40:52 +0100

    generated scene with bunny in middle

--- raytracer/src/main.cpp
+++ raytracer/src/main.cpp
@@ -36,12 +36,12 @@ int main(void) {
   std::cout << "Farben" << glm::to_string(colors) << "\n";
 
 
-  vec3 origin = vec3(-6, 0, -10);
-  vec3 origin_plane = vec3(0, 0, -5);
+  vec3 origin = vec3(-6, 0, 0);
+  vec3 origin_plane = vec3(0, -2, -18);
   //Mesh m = Mesh("data/input/skull.obj",
   //    vec3(0, 15, -20), {.color = vec3(0, 1, 0)});
   Mesh m = Mesh("data/input/bunny_smooth.obj",
-      origin_plane + vec3(0, -1, -10), {.color = vec3(0, 1, 0), .specular=0.2});
+      origin_plane + vec3(0, -0.8, 0), {.color = vec3(0, 1, 0), .specular=0.2});
   Sphere earth = Sphere(origin + vec3(7, 0, 0), 1,{.color = vec3(1, 0, 1), .specular=0.2}, "data/input/earth_uv.png");
   Sphere venus = Sphere(origin + vec3(4, 0, 0), 1.1,{.color = vec3(1, 0, 1), .specular=0.2}, "data/input/textures/planets/venus.jpg");
   Sphere mars = Sphere(origin + vec3(9, 0, 0), 0.5,{.color = vec3(1, 0, 1), .specular=0.2}, "data/input/textures/planets/mars.jpg");
@@ -62,20 +62,21 @@ int main(void) {
   //    vec3(-2, 0, -17), {.color = vec3(1, 0, 0), .mirror = 0.});
   //Mesh c8 = Mesh("data/input/cube.obj",
   //    vec3(+2, 0, -17), {.color = vec3(1, 0, 0), .mirror = 0.3});
-  Plane plane = Plane(origin, vec3(0, 1, 0),
-      {.color = vec3(1 , 1, 1), .mirror = 0.0},
-      {.color = vec3(0.6, 0.6, 0.6), .mirror = 0.0});
+  Plane plane = Plane(origin_plane, vec3(0, 1, 0),
+      {.color = vec3(1 , 1, 1), .mirror = 0.3},
+      {.color = vec3(0.6, 0.6, 0.6), .mirror = 0.3});
+  plane.set_axis(true);
   // Plane plane2 = Plane(vec3(0, 0, -50), vec3(0, 0, -1), vec3(0, 0.5, 1));
-  Pointlight light = Pointlight(origin + vec3(0, 2, 4), 300);
-  Pointlight light1 = Pointlight(origin, 200);
+  Pointlight light = Pointlight(origin + vec3(0, 4, 0), 300);
+  Pointlight light1 = Pointlight(origin_plane + vec3(-3, 5, 4), 250);
   Pointlight light2 = Pointlight(vec3(-6, 8, 1), 150);
 
-  Scene scene = Scene(vec3(0, 0, 0));
+  Scene scene = Scene(vec3(0, 50, 100));
 
   std::cout << "Scene size:" << m.get_size() << std::endl;
 
  
-  scene.get_camera()->set_resolution(1000);
+  scene.get_camera()->set_resolution(500);
   scene.get_camera()->set_sensor_size(1, 1);
 
   scene.set_aliasing(1);
@@ -89,9 +90,8 @@ int main(void) {
   //scene.add_object(&venus);
   //scene.add_object(&mars);
     //
-  //scene.add_object(&plane);
-
   scene.add_object(&m); 
+  scene.add_object(&plane);
   //scene.add_object(&c1);
 
   // generate animation
@@ -99,9 +99,9 @@ int main(void) {
   float samples = 1;
 
   //scene.get_camera()->rotate(origin, vec3(1, 0, 0), -12);
-  //scene.get_camera()->move(vec3(0, 10, 0));
-  //scene.get_camera()->rotate(origin, vec3(0, 1, 0), 15);
-  //scene.update_view_transform();
+  scene.get_camera()->move(vec3(0, 7, 0));
+  scene.get_camera()->rotate(origin, vec3(1, 0, 0), -15);
+  scene.update_view_transform();
 
   for (int i = 0; i < samples; i++) {
     // for aliasing
