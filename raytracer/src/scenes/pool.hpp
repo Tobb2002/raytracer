512f471e15b52e7b50812383de29e85f26db9c6f
Tobi <tobias.vonier@gmx.de>
2024-02-26 19:43:32 +0100

    added billiard scene

--- raytracer/src/scenes/pool.hpp
+++ raytracer/src/scenes/pool.hpp
@@ -1,6 +1,7 @@
 #include "../scene.hpp"
 #include "../objects/sphere.hpp"
 #include "../objects/plane.hpp"
+#include "../object_factory.hpp"
 
 #include <string.h>
 
@@ -12,30 +13,27 @@ Scene get_scene() {
   Plane plane = Plane(origin + vec3(0, -1,0), vec3(0, 1, 0),
       {.color = vec3(0.2 , 1, 0.2),.specular=0, .mirror = 0.0});
 
-  scene.get_camera()->set_resolution(400, 400);
-  scene.get_camera()->set_sensor_size(1, 1);
+  scene.get_camera()->set_resolution(800, 400);
+  scene.get_camera()->set_sensor_size(1, 0.5);
 
-  scene.set_aliasing(1);
+  scene.set_aliasing(4);
   scene.set_tonemapping_value(-1);
 
   // --- add lights ---
-  Pointlight light1 = Pointlight(origin + vec3(-12, 2, 14), 250);
-
-
-  scene.add_light(light1);
+  ObjectFactory factory = ObjectFactory(&scene);
+  
+  factory.new_xy_square_light(origin + vec3(-3, 4, 5),
+                              250,
+                              4,
+                              0.4);
   
 
   // --- add objects ---
-  std::vector<Sphere> spheres;
-  //spheres.reserve(5);
   scene.add_object(plane);
-  //scene.add_object(b1);
-  //scene.add_object(b2);
-  //scene.add_object(b3);
 
   uint count = 0;
   char texture[40];
-  int seq[15] = {1, 9, 2, 3, 4, 10, 11, 7, 14, 4, 5, 13, 15, 6, 12};
+  int seq[15] = {1, 9, 2, 3, 8, 10, 11, 7, 14, 4, 5, 13, 15, 6, 12};
   // textures gehen nicht: 8
 
   for (int i = 1; i <= 5; i++) {
@@ -44,7 +42,7 @@ Scene get_scene() {
         sprintf(texture, "data/input/textures/pool/ball%i.png", seq[count]);
           Sphere b = Sphere(pos,
                         1,
-                        {.color = vec3(1, 0, 1), .specular = 0.6, .mirror=0.3},
+                        {.color = vec3(1, 0, 1), .specular = 0.8, .mirror=0.3},
                         texture);
           // rotate ball randomly
           int rand = 359 * (double) std::rand() / RAND_MAX;
@@ -57,8 +55,8 @@ Scene get_scene() {
 
   }
 
-  scene.get_camera()->move(vec3(0, 4, 0));
-  scene.get_camera()->rotate(origin, vec3(1, 0, 0), -15);
+  scene.get_camera()->move(vec3(2, 3, 0));
+  scene.get_camera()->rotate(origin, vec3(1, 0, 0), -35);
   scene.get_camera()->rotate(origin, vec3(0, 1, 0), 30);
   scene.update_view_transform();
   return scene;
