dd137eba7589f9e8d93d213ed87720ab024f5847
Tobi <tobias.vonier@gmx.de>
2024-02-27 23:32:17 +0100

    added scene to test bvh performance

---
+++ raytracer/src/scenes/performance.hpp
@@ -0,0 +1,34 @@
+#include "../scene.hpp"
+#include "../objects/mesh.hpp"
+#include "../objects/sphere.hpp"
+#include "../objects/plane.hpp"
+#include "../object_factory.hpp"
+
+Scene get_scene() {
+  Scene scene = Scene(vec3(255, 255, 255));
+
+  vec3 origin_plane = vec3(0, -0.7, -2);
+
+  Mesh m = Mesh("data/input/bunny_performance.obj",
+                origin_plane + vec3(0, 0, 0),
+                {.color = vec3(0.8, 0.16, 0), .specular = 0.2});
+
+  Plane plane = Plane(origin_plane, vec3(0, 1, 0),
+      {.color = vec3(1 , 1, 1),.specular=0, .mirror = 0.0},
+      {.color = vec3(0.6, 0.6, 0.6),.specular=0, .mirror = 0.0}, vec2(100,15));
+
+  //plane.set_axis(true);
+  Pointlight light1 = Pointlight(origin_plane + vec3(-3, 2, 4), 250);
+
+  scene.get_camera()->set_resolution(400);
+  scene.get_camera()->set_sensor_size(1, 1);
+
+  scene.set_aliasing(1);
+  scene.set_tonemapping_value(-1);
+
+  scene.add_object(m);
+  scene.add_light(light1);
+
+  return scene;
+}
+
