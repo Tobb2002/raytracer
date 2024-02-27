e8aad976dbaca4f4947b5579bfe9f9a697e5a9f1
Tobi <tobias.vonier@gmx.de>
2024-02-27 17:43:51 +0100

    added texture scene with earth and stars

---
+++ raytracer/src/scenes/texture_planet.hpp
@@ -0,0 +1,40 @@
+#include "../objects/sphere.hpp"
+#include "../objects/plane.hpp"
+#include "../object_factory.hpp"
+
+Scene get_scene() {
+  Scene scene = Scene(vec3(0, 50, 100));
+
+  scene.get_camera()->set_resolution(800, 400);
+  scene.get_camera()->set_sensor_size(1, 0.5);
+
+  scene.set_aliasing(4);
+  scene.set_tonemapping_value(-1);
+
+  vec3 origin = vec3(0, 0, -13);
+
+  Plane plane = Plane(origin + vec3(0, 0, -10), vec3(0, 0, -1),
+      {.color = vec3(1 , 1, 1),.diffuse=0, .ambient=0.6, .specular=0},
+      vec2(14,10),
+      "data/input/textures/planets/background_stars.png");
+
+  Sphere earth = Sphere(origin,
+                        2,
+                        {.specular = 0.2},
+                        "data/input/textures/planets/earth_uv.png");
+  earth.rotate(vec3(0,1,0), -20);
+
+  //plane.set_axis(true);
+  Pointlight light1 = Pointlight(origin + vec3(-6, 2, 4), 320);
+
+  scene.add_light(light1);
+
+
+  scene.add_object(earth);
+  scene.add_object(plane);
+
+
+  return scene;
+}
+
+
