b9b2c666a99ece7c1f523f11a2fd83ac8e2e0402
Tobias Vonier <tobias.vonier@gmx.de>
2024-09-30 14:56:00 +0200

    added grid to bar plots

--- data/experiments/plot_bars.py
+++ data/experiments/plot_bars.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import numpy as np
 
 # amount of triangles to time
@@ -12,7 +13,8 @@ def multibarplot(ax, data, xlabels, ylabels, fill_ratio = 0.8):
     width = fill_ratio/D
 
     for i,(d,ylabel) in enumerate(zip(data,ylabels)):
-        ax.bar(np.arange(l) + (i-(D-1)/2) *width,d, width=width, label=ylabel)
+        ax.bar(np.arange(l) + (i-(D-1)/2) *width,d, width=width, label=ylabel,
+               zorder=3)
 
     ax.set_xticks(np.arange(l), xlabels)
     ax.legend()
@@ -54,24 +56,16 @@ np_data = np.array(data)
 # Plotting the data with lines in different colors
 plt.figure(figsize=(8, 6))
 
-# color = ['darkblue','y','darkred', 'o']
-# if (len(data) > 1):
-#     ax = plt.subplot(111)
-#     w = 0.3
-#     for i in range(len(data)):
-#         ax.bar(groups, data[i], color=color[i], width= 0.2)
-
 #plt.title('Rendering time')
 plt.xlabel(labels[0])
 plt.ylabel(labels[1])
 # plt.legend()
-# plt.grid(True)
-#
-# plt.show()
+plt.grid(zorder=0, axis='y')
 
 #data = np.array([np.arange(i,i+5) for i in range(3)]) + .123
 #plt.figure(figsize=(10,5))
 ax=plt.gca()
+ax.yaxis.set_major_locator(ticker.IndexLocator(base=0.5, offset=0))
 print(data)
 multibarplot(ax, np_data, groups, xgroups)
 plt.show()
